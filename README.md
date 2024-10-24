80b2d39bea66189b0d2088dfe532fa44751635c8
ARC <arcjobgit@proton.me>
2024-10-23 18:55:41 -0600

    Project Upload

---
+++ README.md
@@ -0,0 +1,20 @@
+
+![Logo](https://cdn.discordapp.com/attachments/1191205417527803907/1298802107159150602/logosmall.png?ex=671ae338&is=671991b8&hm=4bfb3a926d8646076ed12b93cc009e1c69b381847a78dcfefb3e6c00549dbe27&)
+
+
+# Smart Recommendations
+An AI powered media recommendation application for android.
+
+
+
+In order to use this version you must have a valid openAI API Key.
+### Navigation
+![Example1](https://cdn.discordapp.com/attachments/1191205417527803907/1298803046968463381/image.png?ex=671ae418&is=67199298&hm=6a7809654857c1fb1787140a85ec35a3c4aa5f9674b25e82b40276b49cabb56c&)
+### User Input
+![Example2](https://cdn.discordapp.com/attachments/1191205417527803907/1298803081768865893/image.png?ex=671ae421&is=671992a1&hm=69c0652c3b20ba69ffbc78e89487da4cb9f933e7ee72f94b7c0d005c0b478e3d&)
+### Settings
+![Example3](https://cdn.discordapp.com/attachments/1191205417527803907/1298803166598664282/image.png?ex=671ae435&is=671992b5&hm=2769fd513a5603709981154ce3edbcc2839467bd78e43768b3bbc9cacffa3927&)
+## License
+
+[Apache-2.0]( https://www.apache.org/licenses/LICENSE-2.0.txt)
+
---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.arc.smartrecommendationsai"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.arc.smartrecommendationsai"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Retrofit for network requests
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Converter for parsing JSON using Gson
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+}
